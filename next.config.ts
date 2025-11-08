462a96340a6dc438ed9da14cdb3d628734ce590b
nirnaykulshreshtha <nirnay.kulshreshtha@sustaininsight.com>
2025-11-08 13:44:54 +0530

    Update Next.js configuration to enable static export, set output directory, and optimize image handling; add trailing slash support for improved URL structure.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export',
-  // distDir: 'out',
+  // comment our for development
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
+  images: { unoptimized: true },
+
 };
 
 export default nextConfig;
